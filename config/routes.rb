35e5562b102117719b5f8daad288e0819a38144d
David Harting <david.e.harting@gmail.com>
2021-02-06 21:48:48 +0000

    Add home and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Chris called his root controller "main", and used the index method
+  # I like "show" here, reserving index for list views. But idk 🤷‍♀️
+  root to: "home#show" # get "/" also works instead of the root helper
+  get "about", to: "about#index"
 end
