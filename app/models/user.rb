0e64a4d51a06511460d5806085fc09023b960d86
David Harting <david.e.harting@gmail.com>
2021-02-07 20:05:30 +0000

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Rails provides has_secure_password
+  # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
+  # This will take care of validations, bcrypt, and  many other things
+  # This also provides virtual fields: "password" and "confirm_password"
+  # on top of our real password_digest column
+  has_secure_password
+end
