da4b6ef983fa5d19a63e3c4fc43117b3d2669c20
David Harting <david.e.harting@gmail.com>
2021-02-13 18:34:29 +0000

    17: login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,30 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    p = signin_params
+    user = User.find_by(email: p[:email])
+    # Authenticate comes from has_secure_password on our user model
+    if user.present? && user.authenticate(p[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      redirect_to signin_path
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out"
+  end
+
+  private
+
+  def signin_params
+    params.require(:email)
+    params.require(:password)
+    params.permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails
 
   get "signup", to: "registrations#new"
   post "signup", to: "registrations#create"
+
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create"
   delete "signout", to: "sessions#destroy"
 end
