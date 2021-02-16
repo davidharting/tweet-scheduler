7c18fd6dbd2599db633b9f4f7d67e837b01159ed
David Harting <david.e.harting@gmail.com>
2021-02-16 13:40:18 +0000

    29: Tweet index and new

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,22 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Current.user.tweets
+    @has_connected_account = Current.user.twitter_accounts.any?
+  end
+
+  def new
+    @tweet = Tweet.new
+    @twitter_accounts = Current.user.twitter_accounts
+  end
+
+  def create
+    puts(tweet_params)
+    redirect_to new_tweet_path, alert: "Create not implemented"
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body, :twitter_account_id, :publish_at)
   end
 end
